9326617d3c49370774d287e70a17332a45d55ce2
sachin27verma <97831442+sachin27verma@users.noreply.github.com>
2024-05-27 23:26:59 +0530

    added cors

--- server/app.py
+++ server/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 from route_planning import fetch_route
+from flask_cors import  CORS
 
 app = Flask(__name__)
+CORS(app) 
 
 @app.route('/',methods=['GET'])
 def return_home():
